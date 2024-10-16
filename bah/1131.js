b8f3012aeef52d05ff0e350b5872e7bac67f3455
GiovanniGCastagna <giovanni.gcastagna@gmail.com>
2024-10-16 19:26:35 -0300

    ""

--- bah/1131.js
+++ bah/1131.js
@@ -1,18 +1,22 @@
-const { match } = require("assert");
-
 var input = require("fs").readFileSync("./dev/stdin/file.txt", "utf8");
 var lines = input.split("\n");
 
 const valueTaker = (line) => line.split(" ").map((a) => +a);
 
-let goals = ([] = valueTaker(lines.shift()));
-let matchInput = +lines.shift();
+let goals = [];
+let matchInput;
 
 let interWins = 0;
 let gremioWins = 0;
 let draws = 0;
+console.log("Novo grenal (1-sim 2-nao)");
+let gameCounter = 1;
 
-while (matchInput == 1) {
+while (matchInput != 2) {
+  goals = valueTaker(lines.shift());
+  matchInput = +lines.shift();
+  console.log("Novo grenal (1-sim 2-nao)");
+  gameCounter++;
   if (goals[0] > goals[1]) {
     interWins++;
   } else if (goals[1] > goals[0]) {
@@ -20,6 +24,14 @@ while (matchInput == 1) {
   } else if (goals[0] == goals[1]) {
     draws++;
   }
-  goals = valueTaker(lines.shift());
-  matchInput = +lines.shift();
+  console.log(interWins, gremioWins, draws);
+}
+
+console.log(`Inter:${interWins}`);
+console.log(`Gremio:${gremioWins}`);
+console.log(`Empates:${draws}`);
+if (interWins > gremioWins) {
+  console.log("Inter venceu mais");
+} else {
+  console.log("Gremio venceu mais");
 }
